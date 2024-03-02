248b456c694e3fa3b476532b66f54dcb4de9aa8f
Abhiraj Trivedi <trivediabhiraj7@gmail.com>
2024-03-02 23:42:34 +0530

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+const path = require("path");
+
+const staticPath = path.join(__dirname, "./public");
+app.use(express.static(staticPath));
+app.set("view engine","html")
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public","home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public","login.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public","about.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public","register.html"));
+});
+
+app.get("/tandc", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public","tandc.html"));
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
