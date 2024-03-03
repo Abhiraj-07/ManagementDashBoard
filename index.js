8ffcc0ad76285075aa702d4533ed158321faf140
Abhiraj Trivedi <trivediabhiraj7@gmail.com>
2024-03-03 11:05:17 +0530

    commit

--- index.js
+++ index.js
@@ -7,26 +7,30 @@ const path = require("path");
 
 const staticPath = path.join(__dirname, "./public");
 app.use(express.static(staticPath));
-app.set("view engine","html")
+app.set("view engine", "html");
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public","home.html"));
+  res.sendFile(path.join(__dirname, "./public", "register.html"));
+});
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "home.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public","login.html"));
+  res.sendFile(path.join(__dirname, "./public", "login.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public","about.html"));
+  res.sendFile(path.join(__dirname, "./public", "about.html"));
 });
 app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public","register.html"));
+  res.sendFile(path.join(__dirname, "./public", "register.html"));
 });
 
 app.get("/tandc", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public","tandc.html"));
+  res.sendFile(path.join(__dirname, "./public", "tandc.html"));
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
